37ba11a2c2e3997d882cc23ff4f8d4341ba30ca9
mahmoodalgbory <algbormd@mail.uc.edu>
2022-09-22 14:58:11 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        int age = 22;
+
+        if (age >= 21)
+            System.out.println("you can get a wristband!");
+
+    }
+}
+
+
+
+
+// main ()
+//	num age = 0
+//
+//	Output "Enter your age: "
+//	input age
+//
+//	if age >= 21 then
+//		output "You can get a wristband.  "
+//	endIf
+//
+//  return
